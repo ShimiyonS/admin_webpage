0aa6bec531eeb238f0c93b707fa3fcdfb03524d8
ShimiyonS <shimjnss@gmail.com>
2023-06-29 17:08:57 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
-import { Route, Routes } from 'react-router-dom';
+//import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Forgtopassword from './Forgto_password';
-import Singup from './Singup_page';
+//import Singup from './Singup_page';
 
 function App() {
   return (<>
-    <Routes>
-      <Route path='/users/reset_password/:id/:token' element={<Forgtopassword/>}/>
-      <Route path='/singup' element={<Singup/>}/>
-    </Routes>
+   <Forgtopassword/>
     </>
   );
 }
